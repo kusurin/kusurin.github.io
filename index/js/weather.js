9b96fc2a7780676be8c8dabd2590de01413dd5c0
kusurin <kusu_rin@sjtu.edu.cn>
2023-12-10 23:54:10 +0800

    fix:ajax sync cause loading stop

--- index/js/weather.js
+++ index/js/weather.js
@@ -1,19 +1,19 @@
 function getTemperature() {
     var weather;
+    var defer = $.Deferred();
     $.ajax({
-        async: false,
         type: 'GET',
         url: 'https://api.open-meteo.com/v1/dwd-icon',
         data: 'latitude=31.2222&longitude=121.4581&hourly=temperature_2m&forecast_days=1',
         dataType: 'JSON',
         error: function () {
-            return null;
+            defer.resolve(null);
         },
         success: function (responseData) {
-            weather = Array.from(responseData.hourly.temperature_2m).reduce((ante,post)=>{
+            defer.resolve(Array.from(responseData.hourly.temperature_2m).reduce((ante, post) => {
                 return ante + post;
-            }) / responseData.hourly.temperature_2m.length;
+            }) / responseData.hourly.temperature_2m.length);
         }
     });
-    return weather;
+    return defer.promise();
 }
