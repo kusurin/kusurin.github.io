57984433421a63eb0d6e06e48ee605e894bcd5f8
kusurin <kusu_rin@sjtu.edu.cn>
2024-03-14 09:55:09 +0800

    promote: 独立存储预数据

---
+++ toys/HELLDIVER/js/balls.js
@@ -0,0 +1,8 @@
+var balls_colle = [
+    { name: "SOS", commands: "udru", class: "固定战略", icon: "https://pic1.zhimg.com/v2-c289d7bf23c58b54ef2d9a3d06d8b7e7_xl.jpg?source=32738c0c" },
+    { name: "支援", commands: "udrlu", class: "固定战略", icon: "https://pic1.zhimg.com/v2-c289d7bf23c58b54ef2d9a3d06d8b7e7_xl.jpg?source=32738c0c" },
+    { name: "重新补给", commands: "ddur", class: "固定战略", icon: "https://pic1.zhimg.com/v2-c289d7bf23c58b54ef2d9a3d06d8b7e7_xl.jpg?source=32738c0c" },
+    { name: "“飞鹰”500KG炸弹", commands: "urddd", class: "飞鹰系列", icon: "https://pic1.zhimg.com/v2-c289d7bf23c58b54ef2d9a3d06d8b7e7_xl.jpg?source=32738c0c" },
+    { name: "轨道炮攻击", commands: "ruddr", class: "轨道轰炸系列", icon: "https://pic1.zhimg.com/v2-c289d7bf23c58b54ef2d9a3d06d8b7e7_xl.jpg?source=32738c0c" },
+    { name: "撕裂者核弹", commands: "rulrdud", class: "轨道轰炸系列", icon: "https://pic1.zhimg.com/v2-c289d7bf23c58b54ef2d9a3d06d8b7e7_xl.jpg?source=32738c0c" }
+]
