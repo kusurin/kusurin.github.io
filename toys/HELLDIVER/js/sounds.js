aa62a080062a4a52dd99e2d72b19642416abd309
kusurin <kusu_rin@sjtu.edu.cn>
2024-03-19 17:40:55 +0800

    fix: too short delay before audio dom recover

--- toys/HELLDIVER/js/sounds.js
+++ toys/HELLDIVER/js/sounds.js
@@ -3,4 +3,5 @@ let sounds_colle = [
     { name: "button", dom: new Audio('./others/button.mp3'), time: 182, playing: false },
     { name: "button", dom: new Audio('./others/button.mp3'), time: 182, playing: false },
     { name: "button", dom: new Audio('./others/button.mp3'), time: 182, playing: false },
+    { name: "button", dom: new Audio('./others/button.mp3'), time: 182, playing: false },
     { name: "button", dom: new Audio('./others/button.mp3'), time: 182, playing: false }]
